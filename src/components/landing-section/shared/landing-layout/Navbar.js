9c62e8c78d7b2e6723c56c564d7730a492b8fe78
Osama Alsadi <e.Osama@dttmoj.onmicrosoft.com>
2022-03-14 08:22:32 +0300

    made the navbar component customizable

--- src/components/landing-section/shared/landing-layout/Navbar.js
+++ src/components/landing-section/shared/landing-layout/Navbar.js
@@ -1,9 +1,51 @@
 import NavbarResponsive from "../../../../lib/components/NavbarResponsive";
 
+const links = [
+  {
+    title: "Github",
+    url: "//github.io/jo_geek",
+  },
+  {
+    title: "Stackoverflow",
+    url: "http://stackoverflow.com/users/4084003/",
+  },
+  {
+    title: "Github",
+    url: "//github.io/jo_geek",
+  },
+  {
+    title: "Stackoverflow",
+    url: "http://stackoverflow.com/users/4084003/",
+  },
+  {
+    title: "Github",
+    url: "//github.io/jo_geek",
+  },
+  {
+    title: "Stackoverflow",
+    url: "http://stackoverflow.com/users/4084003/",
+  },
+];
+
+const bgColorClass = "tw-bg-bt-beige-1";
+const textColorClass = "tw-text-black/80";
+const linkHoverColorClass = "hover:tw-bg-black/20";
+const hamburgerSmBgColorClass = "sm-max:tw-bg-bt-dark-beige";
+const hamburgerSmHoverBgColorClass = "sm-max:hover:tw-bg-bt-dark-beige";
+const hamburgerSmBorderColorClass = "sm-max:tw-border-black/80";
+
 const Navbar = () => {
   return (
     <div>
-      <NavbarResponsive />
+      <NavbarResponsive
+        bgColorClass={bgColorClass}
+        textColorClass={textColorClass}
+        linkHoverColorClass={linkHoverColorClass}
+        hamburgerSmBgColorClass={hamburgerSmBgColorClass}
+        hamburgerSmHoverBgColorClass={hamburgerSmHoverBgColorClass}
+        hamburgerSmBorderColorClass={hamburgerSmBorderColorClass}
+        links={links}
+      />
     </div>
   );
 };
--- src/lib/components/NavbarResponsive.js
+++ src/lib/components/NavbarResponsive.js
@@ -1,7 +1,23 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const links = [
+const linksSample = [
+  {
+    title: "Github",
+    url: "//github.io/jo_geek",
+  },
+  {
+    title: "Stackoverflow",
+    url: "http://stackoverflow.com/users/4084003/",
+  },
+  {
+    title: "Github",
+    url: "//github.io/jo_geek",
+  },
+  {
+    title: "Stackoverflow",
+    url: "http://stackoverflow.com/users/4084003/",
+  },
   {
     title: "Github",
     url: "//github.io/jo_geek",
@@ -12,42 +28,66 @@ const links = [
   },
 ];
 
+const bgColorClassSample = "tw-bg-bt-beige-1";
+const textColorClassSample = "tw-text-black/80";
+const linkHoverColorClassSample = "hover:tw-bg-black/20";
+const hamburgerSmBgColorClassSample = "sm-max:tw-bg-bt-dark-beige";
+const hamburgerSmHoverBgColorClassSample = "sm-max:hover:tw-bg-bt-dark-beige";
+const hamburgerSmBorderColorClassSample = "sm-max:tw-border-black/80";
+
 /// A customizable responsive navbar component that depends on react-router-dom and tailwind CSS
-const NavbarResponsive = () => {
+/// Also depends on a custom tailwind screen size using max-size:
+/// (sm-max) which should be the inverse of (md)
+
+const NavbarResponsive = ({
+  bgColorClass = bgColorClassSample,
+  textColorClass = textColorClassSample,
+  linkHoverColorClass = linkHoverColorClassSample,
+  hamburgerSmBgColorClass = hamburgerSmBgColorClassSample,
+  hamburgerSmHoverBgColorClass = hamburgerSmHoverBgColorClassSample,
+  hamburgerSmBorderColorClass = hamburgerSmBorderColorClassSample,
+  links = linksSample,
+}) => {
   const [showDropdown, setShowDropdown] = useState(false);
-  const linkClass = `hover:tw-bg-black/20 tw-inline-flex-center tw-px-10px sm-max:tw-block sm-max:tw-w-full
+  const linkClass = `${linkHoverColorClass} tw-inline-flex-center tw-px-10px sm-max:tw-block sm-max:tw-w-full
     sm-max:tw-py-5px md:tw-h-full`;
   const spanClass = `sm-max:tw-block sm-max:tw-w-25px sm-max:tw-h-10px 
-    sm-max:tw-border-t-2 sm-max:tw-border-solid sm-max:tw-border-black/80`;
-  const labelsClass = showDropdown ? "sm-max:tw-bg-bt-dark-beige" : "";
-  const navLinksClass = showDropdown
+    sm-max:tw-border-t-2 sm-max:tw-border-solid`;
+  const spanBorderClass = hamburgerSmBorderColorClass;
+  const labelsClass = showDropdown ? hamburgerSmBgColorClass : "";
+  const navLinksClass__ = showDropdown
     ? "sm-max:tw-max-h-500px sm-max:tw-overflow-y-auto"
     : "sm-max:tw-max-h-0 sm-max:tw-overflow-y-hidden";
+  const navLinksClass = showDropdown
+    ? "sm-max:tw-max-h-500px"
+    : "sm-max:tw-max-h-0";
+  //const hamburgerSmHoverBgColorClass = "sm-max:hover:" + hamburgerBgColorClass;
+  //const hamburgerSmBgColorClass = "sm-max:" + hamburgerBgColorClass;
 
   return (
-    <div className="tw-bg-bt-beige-1 tw-h-50px tw-relative tw-flex tw-items-center tw-text-black/80">
-      {/* <input type="checkbox" id="nav-check" className="tw-hidden tw-group" /> */}
-      <div className="tw-inline-flex-center tw-h-full tw-px-10px">
-        <div className="tw-inline-block tw-text-20px ">Budget Tracker</div>
+    <div
+      className={`${bgColorClass} ${textColorClass} tw-h-50px tw-relative tw-flex tw-items-center`}
+    >
+      <div className={`tw-inline-flex-center tw-h-full tw-px-10px`}>
+        <div className={`tw-inline-block tw-text-20px`}>Budget Tracker</div>
       </div>
       <div
-        className="tw-hidden sm-max:tw-inline-block sm-max:tw-absolute 
-        sm-max:tw-top-0 sm-max:tw-right-0"
+        className={`tw-hidden sm-max:tw-inline-block sm-max:tw-absolute 
+        sm-max:tw-top-0 sm-max:tw-right-0`}
       >
         <label
-          //   for="nav-check"
           onClick={() => setShowDropdown(!showDropdown)}
-          className={`${labelsClass} sm-max:tw-inline-block sm-max:tw-w-50px sm-max:tw-h-50px
-            sm-max:tw-p-15px sm-max:hover:tw-bg-black/20`}
+          className={`${labelsClass} ${hamburgerSmHoverBgColorClass} sm-max:tw-inline-block sm-max:tw-w-50px sm-max:tw-h-50px
+            sm-max:tw-p-15px`}
         >
-          <span className={spanClass}></span>
-          <span className={spanClass}></span>
-          <span className={spanClass}></span>
+          <span className={`${spanClass} ${spanBorderClass}`}></span>
+          <span className={`${spanClass} ${spanBorderClass}`}></span>
+          <span className={`${spanClass} ${spanBorderClass}`}></span>
         </label>
       </div>
       <div
-        className={`${navLinksClass} tw-grow tw-text-18px tw-inline-flex tw-justify-end tw-items-stretch
-            sm-max:tw-block sm-max:tw-absolute sm-max:tw-bg-bt-dark-beige sm-max:tw-w-full md:tw-h-full
+        className={`${navLinksClass} ${hamburgerSmBgColorClass} tw-grow tw-text-18px tw-inline-flex tw-justify-end tw-items-stretch
+            sm-max:tw-block sm-max:tw-absolute sm-max:tw-w-full md:tw-h-full sm-max:tw-overflow-hidden
             sm-max:tw-transition-all sm-max:tw-duration-300 sm-max:tw-ease-in
             sm-max:tw-top-50px sm-max:tw-left-0`}
       >
@@ -60,37 +100,6 @@ const NavbarResponsive = () => {
             );
           })}
         </>
-        {/* <a className={linkClass} href="//github.io/jo_geek" target="_blank">
-          Github
-        </a>
-        <a
-          className={linkClass}
-          href="http://stackoverflow.com/users/4084003/"
-          target="_blank"
-        >
-          Stackoverflow
-        </a>
-        <a
-          className={linkClass}
-          href="https://in.linkedin.com/in/jonesvinothjoseph"
-          target="_blank"
-        >
-          LinkedIn
-        </a>
-        <a
-          className={linkClass}
-          href="https://codepen.io/jo_Geek/"
-          target="_blank"
-        >
-          Codepen
-        </a>
-        <a
-          className={linkClass}
-          href="https://jsfiddle.net/user/jo_Geek/"
-          target="_blank"
-        >
-          JsFiddle
-        </a> */}
       </div>
     </div>
   );
