eab057ae0817065b878b31d2eb5114921a96b564
Osama Alsadi <diacubic@hotmail.com>
2022-09-23 00:57:41 +0300

    ...

--- src/components/app/pages/SettingsPage.js
+++ src/components/app/pages/SettingsPage.js
@@ -36,7 +36,7 @@ const SettingsPage = () => {
           <ControlledInput
             className="tw-flex-grow"
             value={currencySymbol}
-            maxLength={8}
+            maxLength={6}
             onChange={(value) => setCurrencySymbol(value)}
           />
           <XButton className="" onClick={updateCurrencySymbol}>
