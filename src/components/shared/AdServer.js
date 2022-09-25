fd0652a579a807e7929d48c431ce51cf0787d53c
Osama Alsadi <diacubic@hotmail.com>
2022-09-25 21:46:09 +0300

    Added Amazon Associate Banner

---
+++ src/components/shared/AdServer.js
@@ -0,0 +1,16 @@
+const AdServer = () => {
+  return (
+    <div className="alignleft">
+      <script type="text/javascript">
+        amzn_assoc_ad_type = "banner"; amzn_assoc_marketplace = "amazon"; amzn_assoc_region = "US";
+        amzn_assoc_placement = "assoc_banner_placement_default"; amzn_assoc_campaigns =
+        "amazonfashion"; amzn_assoc_banner_type = "category"; amzn_assoc_isresponsive = "true";
+        amzn_assoc_banner_id = "16WMC1S47DJG0A0MEW82"; amzn_assoc_tracking_id = "budgeteeer-20";
+        amzn_assoc_linkid = "c43d7c788849d4015cdcdd808db9138c";
+      </script>
+      <script src="//z-na.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&Operation=GetScript&ID=OneJS&WS=1"></script>
+    </div>
+  );
+};
+
+export default AdServer;
--- src/components/shared/AppLayout.js
+++ src/components/shared/AppLayout.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 import SideBar from "./components/SideBar";
 import Header from "./components/Header";
+import AddServer from "./AdServer";
 
-import { transactionTypeEnum } from "../../constants/enums";
 import Footer from "./Footer";
 
 const AppLayout = ({
@@ -23,6 +23,7 @@ const AppLayout = ({
       <SideBar showPopup={showMobileMenu} onClose={() => setShowMobileMenu(!showMobileMenu)} />
       <div className="tw-grow tw-flex tw-max-h-[100vh] lg:tw-overflow-auto">
         <div className="tw-flex tw-flex-col tw-items-stretch tw-w-full tw-h-full tw-pt-12px tw-px-16px lg:tw-pt-32px lg:tw-px-40px">
+          <AddServer />
           <Header
             selectedTxType={selectedTxType}
             setSelectedTxType={setSelectedTxType}
